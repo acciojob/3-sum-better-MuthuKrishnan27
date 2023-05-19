5c78051eb88ad2520b44a98b6ad6fad811d62349
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-05-19 08:45:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let a = [];
+	let sum = 0;
+	for(let i=0;i<arr.length-2;i++){
+		let k = i+3;
+		for(let j=i;j<arr.length && j<k;j++){
+			sum+=arr[j];
+		}
+		a.push(sum);
+	}
+	let z = [];
+	let min;
+	for(let i=0;i<a.length;i++){
+		if(a[i]<0){
+			z[i] = z[i] * -1;
+		}
+		z[i] = z[i] - target;
+	}
+
+	let min_index = 0;
+	let m = z[0];
+	for(let i=0;i<z.length;i++){
+		for(let j=i+1;j<z.length;j++){
+			if(z[j]<m){
+				m=z[j];
+				min_index=j;
+			}
+		}
+	}
+	min = a[min_index];
+	return min;
 }
 
 module.exports = threeSum;
